77cb3a34e19c222643dce1574f457f7b2d7fbdf6
Rorisang Putu <rorisangputu@gmail.com>
2025-01-03 23:06:50 +0200

    python course

---
+++ Lesson01/data_types.py
@@ -0,0 +1,12 @@
+#Variables
+name ='John' # string
+age=15 #int
+isNew= True #boolean
+score = 80.5 #floating point
+
+
+print(type(name)) #checking variable type
+name = str(name) #explicit type casting
+
+print(name)
+
