cd2ac1699929d865c0b29d4c7c099055fbc92426
Rorisang Putu <rorisangputu@gmail.com>
2025-01-03 23:13:35 +0200

    doing input exeercise

--- Lesson02/input.py
+++ Lesson02/input.py
@@ -1,2 +1,9 @@
-name = input('What is your name? ') #input stores user input which can be saved to a variable
-print('Hi ' + name) #printing input that was saved to variables
+#name = input('What is your name? ') #input stores user input which can be saved to a variable
+#print('Hi ' + name) #printing input that was saved to variables
+
+#Exercise
+
+name = input('What is your name? ')
+favColour = input('What is your favourite color? ')
+
+print(name + ' likes ' + favColour)
