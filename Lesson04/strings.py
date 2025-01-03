06574280935e4d2d53edc0cf466513d68a3cc6e7
Rorisang Putu <rorisangputu@gmail.com>
2025-01-03 23:30:46 +0200

    strings

---
+++ Lesson04/strings.py
@@ -0,0 +1,19 @@
+course = 'Python for "Beginners"' #using double quotes in between single quotes
+print(course)
+
+dub_course = "Python's beginner course" #using single quote for apostrophe in between double quotes
+print(dub_course)
+
+#multi line string declaration with 3 quotes
+
+tri_course = '''Dear Mabel,
+
+How are you doing? It has been a while
+
+The family is doing well.
+
+Love, Rorisang <3
+
+'''
+
+print(tri_course)
