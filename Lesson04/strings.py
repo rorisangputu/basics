1ec6f0b13e1ee71dad2309232095ed43ad8967a9
Rorisang Putu <rorisangputu@gmail.com>
2025-01-03 23:34:15 +0200

    accessing string letters as if string were array

--- Lesson04/strings.py
+++ Lesson04/strings.py
@@ -1,19 +1,21 @@
 course = 'Python for "Beginners"' #using double quotes in between single quotes
+print(course[0]) #accessing first letter of string
+print(course[-3]) #accessing second last letter in string
 print(course)
 
-dub_course = "Python's beginner course" #using single quote for apostrophe in between double quotes
-print(dub_course)
+#dub_course = "Python's beginner course" #using single quote for apostrophe in between double quotes
+#print(dub_course)
 
 #multi line string declaration with 3 quotes
 
-tri_course = '''Dear Mabel,
+#tri_course = '''Dear Mabel,
 
-How are you doing? It has been a while
+#How are you doing? It has been a while
 
-The family is doing well.
+#The family is doing well.
 
-Love, Rorisang <3
+#Love, Rorisang <3
 
-'''
+#'''
+#print(tri_course)
 
-print(tri_course)
