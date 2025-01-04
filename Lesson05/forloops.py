39adfa6fca223e32d04167884adeb56f22061707
Rorisang Putu <rorisangputu@gmail.com>
2025-01-04 15:37:32 +0200

    printing range of numbers up to a certain point

--- Lesson05/forloops.py
+++ Lesson05/forloops.py
@@ -1,4 +1,7 @@
-word = 'Python'
-names = ['Rorisang', 'Mpho', 'Mike', 'Thomas']
-for name in names:
-    print(str(names.index(name) + 1) + ' '+ name)
+#word = 'Python'
+#names = ['Rorisang', 'Mpho', 'Mike', 'Thomas']
+#for name in names:
+#    print(str(names.index(name) + 1) + ' '+ name)
+
+for num in range(25): #printing numbers from 0 - 24
+    print(str(num +1)) # adding 1 to print from 1 to 25
