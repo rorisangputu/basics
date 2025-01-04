094ebf88da5ca1ff90643e065bbbb07153e9aaa4
Rorisang Putu <rorisangputu@gmail.com>
2025-01-04 15:43:02 +0200

    performing arthimetic operations on array values

--- Lesson05/forloops.py
+++ Lesson05/forloops.py
@@ -3,5 +3,15 @@
 #for name in names:
 #    print(str(names.index(name) + 1) + ' '+ name)
 
-for num in range(25): #printing numbers from 0 - 24
-    print(str(num +1)) # adding 1 to print from 1 to 25
+#for num in range(25): #printing numbers from 0 - 24
+#    print(str(num +1)) # adding 1 to print from 1 to 25
+
+#for numsta in range(1,10): ##from 1 to 9
+#    print(numsta)
+
+prices = [10, 20, 30]
+total_price = 0
+for price in prices:
+    total_price += price
+
+print('The total cost of the cart: ' + str(total_price))
