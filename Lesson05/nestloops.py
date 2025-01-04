a51dcbb10eec5c9f3f2b9158e668863149c6f1b6
Rorisang Putu <rorisangputu@gmail.com>
2025-01-04 16:08:34 +0200

    nested loops

---
+++ Lesson05/nestloops.py
@@ -0,0 +1,10 @@
+#for x in range(4):
+#    for y in range(3):
+#        print(f"({x}, {y})")
+
+numbers = [5, 2, 5, 2, 2]
+for number in numbers:
+    output = ''
+    for count in range(number):
+        output += 'x'
+    print(output)
