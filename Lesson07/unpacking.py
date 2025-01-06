53d1b17b03f7afe78aa9d8ab45e41c93b844eec3
Rorisang Putu <rorisangputu@gmail.com>
2025-01-06 15:45:04 +0200

    simply unpacking tuple whithout repeating words

---
+++ Lesson07/unpacking.py
@@ -0,0 +1,11 @@
+#UNPACKING
+
+#defining a tuple
+coordinates = (1, 2, 3)
+
+#x = coordinates[0] # Too much
+#y = coordinates[1] # repeatition
+#z = coordinates[2] # in code
+
+x, y, z = coordinates
+print(x) # prints 1
