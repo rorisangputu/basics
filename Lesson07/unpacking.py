67f8a1c41b8f0b1b3a81a1508ef4adb0289d107c
Rorisang Putu <rorisangputu@gmail.com>
2025-01-06 15:45:59 +0200

    comments

--- Lesson07/unpacking.py
+++ Lesson07/unpacking.py
@@ -7,5 +7,7 @@ coordinates = (1, 2, 3)
 #y = coordinates[1] # repeatition
 #z = coordinates[2] # in code
 
-x, y, z = coordinates
+x, y, z = coordinates #short-hand assignment
 print(x) # prints 1
+
+#unpacking also works with lists []
