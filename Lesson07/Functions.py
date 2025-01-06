5cdea5c785c72a7521b002d3c40b4773cbcb499e
Rorisang Putu <rorisangputu@gmail.com>
2025-01-06 16:57:21 +0200

    defining keword argument positions

--- Lesson07/Functions.py
+++ Lesson07/Functions.py
@@ -7,7 +7,9 @@ greet_user()
 
 #Functions with Parameters
 def greet_person(word):
-    print("Hello " + word)
+    print(f"Hello {word}") #using formatted string
 
 
 greet_person("Rorisang")
+greet_person("Matt")
+greet_person("Kole")
---
+++ Lesson07/keyword_args.py
@@ -0,0 +1,5 @@
+def greet_user (first_name, last_name): #default args positions
+    print(f"Hello {first_name} {last_name}!")
+    print('Welcome aboard')
+
+greet_user(last_name="Bumz", first_name="Mike") #specifying keyword instead of default args postion
