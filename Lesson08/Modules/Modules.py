cd3f7eb948e80d9ec7ce06c8089617ff2cca2e51
Rorisang Putu <rorisangputu@gmail.com>
2025-01-06 23:06:45 +0200

    working with packages

--- Lesson08/Modules/Modules.py
+++ Lesson08/Modules/Modules.py
@@ -1,6 +1,7 @@
 import converters
 from converters import kg_to_lbs #ctrl + space
 
+
 from utils import find_max
 
 weight = int(kg_to_lbs(47))
@@ -10,3 +11,4 @@ numbers = [2, 32, 17, 28, 9, 8, 65, 45]
 maxNum = find_max(numbers)
 print(maxNum)
 print(max(numbers)) ## does the same as top
+
---
+++ app.py
@@ -0,0 +1,6 @@
+import ecommerce.shipping
+from ecommerce.shipping import calc_shipping
+
+ecommerce.shipping.calc_shipping()
+
+calc_shipping()
