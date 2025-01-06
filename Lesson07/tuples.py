bd22b77eced2bcddca85993b451e0501a759596a
Rorisang Putu <rorisangputu@gmail.com>
2025-01-06 15:41:02 +0200

    tuples

---
+++ Lesson07/tuples.py
@@ -0,0 +1,7 @@
+#TUPLES ARE SIMILAR TO LISTS BUt ARE IMMUTIBLE
+numbers = (1, 2, 3) #Tuples use parenthesis () and Lists use square brackets []
+#numbers[0] =10 # will get an error
+print(numbers[2]) #will print the value in position
+
+#Tuples cannot be changed or re-assigned. They only have two methods
+# Great for array that shouldn't be changed in program
